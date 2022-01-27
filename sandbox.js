fa39bafbb48d583b7b5c774289d657e6641fe389
itishprasad30 <itishprasad20@gmail.com>
2022-01-27 10:33:35 +0530

    Typescript basics

---
+++ sandbox.js
@@ -0,0 +1,59 @@
+var charecter = 'superBowl';
+console.log(charecter);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
+var anothervalue = 'The weeknd';
+anothervalue = 'abel tesfaye';
+// anothervalue = 12   its will give error
+console.log(anothervalue);
+// arrow function 
+var circle = function (diameter) {
+    return Math.floor(diameter * Math.PI);
+};
+console.log(circle(5));
+//arrays and objects
+var names = ['abel', 'taylor', 'dua'];
+names.push('ed shreen');
+// names.push(12) its will show error 
+names[0] = 'the weeknd';
+// names[0] = 23
+console.log(names);
+var numbers = [2, 34, 56, 7, 9];
+numbers.push(233);
+// numbers.push('some') its will show error
+console.log(numbers);
+var mixed = [23, 'some', true];
+mixed.push('add');
+mixed.push(111);
+mixed.push(false);
+console.log(mixed);
+var person = {
+    name: 'pintu sahoo',
+    age: 22,
+    single: true
+};
+person.single = true;
+console.log(person);
+// explicit types
+var char;
+var age;
+var type;
+// age = 'some'
+age = 12;
+// array
+var ninja = ['nim', 'vbal'];
+ninja.push('hello');
+console.log(ninja);
+//union mixed
+var mixedVal;
+mixedVal = ['some', 12, 'false', 433];
+// Object
+var newObj;
+newObj = {
+    name: 'The Weeknd',
+    age: 32
+};
+console.log(newObj);
---
+++ sandbox.ts
@@ -0,0 +1,107 @@
+const charecter = 'superBowl'
+
+console.log(charecter);
+
+
+const inputs = document.querySelectorAll('input')
+console.log(inputs);
+
+inputs.forEach(input => {
+    console.log(input);
+})
+
+
+
+let anothervalue = 'The weeknd'
+
+anothervalue = 'abel tesfaye'
+// anothervalue = 12   its will give error
+
+console.log(anothervalue);
+
+// arrow function 
+const circle = (diameter:number) =>{
+    return Math.floor(diameter * Math.PI)
+}
+
+
+console.log(circle(5));
+
+
+//arrays and objects
+let names = ['abel','taylor','dua']
+
+names.push('ed shreen')
+// names.push(12) its will show error 
+
+names[0] ='the weeknd'
+
+// names[0] = 23
+console.log(names);
+
+
+
+let numbers = [2,34,56,7,9]
+numbers.push(233)
+// numbers.push('some') its will show error
+console.log(numbers);
+
+let mixed = [23,'some',true]
+
+mixed.push('add')
+mixed.push(111)
+mixed.push(false)
+
+console.log(mixed);
+
+// Objects 
+interface Person {
+    name:string,
+    age:number,
+    single?:boolean
+}
+
+let person: Person  = {
+    name:'pintu sahoo',
+    age:22,
+    single:true
+}
+person.single = true
+
+console.log(person);
+
+
+// explicit types
+let char: string
+let age: number
+let type : boolean
+
+// age = 'some'
+age = 12
+
+
+// array
+
+let ninja : string[]  = ['nim','vbal']
+
+ninja.push('hello')
+console.log(ninja);
+
+//union mixed
+
+let mixedVal : (string| number )[]
+
+mixedVal = ['some',12,'false',433]
+
+
+// Object
+let newObj: {
+    name:string,
+    age:number,
+}
+
+newObj= {
+    name:'The Weeknd',
+    age:32
+}
+console.log(newObj);
